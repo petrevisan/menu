d5ec10f37aa473c3fa9e940b5492eb4e44d56d3e
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2025-06-27 19:23:28 -0400

    feat: atualiza main.py para adicionar novas rotas

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 import pandas as pd
 from fastapi import FastAPI
-from routes import sheets
+from routes.spreadsheets import get_spreadsheet_data
+from routes.users import create_user
 
 app = FastAPI()
 
-app.include_router(sheets.router)
+app.include_router(get_spreadsheet_data.router)
+app.include_router(create_user.router)
 
 @app.get("/")
 async def root():
